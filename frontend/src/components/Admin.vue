8435520960dfd9f03ac9e668dbda964a6aa06335
omitent <mickl0102@outlook.com>
2019-08-11 12:19:35 -0400

    WIP: admin dashboard

--- frontend/src/components/Admin.vue
+++ frontend/src/components/Admin.vue
@@ -1,9 +1,33 @@
 <template>
 <v-container>
     <v-layout>
-        <v-flex xs10 text-center>
-            <h1>Admin</h1>
-            <div>Welcome {{ username }}</div>
+        <v-flex xs12 text-center>
+            <h1>Admin Dashboard</h1>
+            <v-tabs
+              dark
+              v-model="currentTab"
+              centered
+            >
+                <v-tabs-slider></v-tabs-slider>
+                <v-tab
+                    v-for="tab in tabs"
+                    :key="tab.name"
+                    :href="`#${tab.name}`"
+                >
+                    {{ tab.title }}
+                </v-tab>
+                <v-tab-item
+                    v-for="tab in tabs"
+                    :key="tab.name"
+                    :value="`${tab.name}`"
+                >
+                    <v-card flat>
+                        <v-card-text>
+                            {{ tab.name }}
+                        </v-card-text>
+                    </v-card>
+                </v-tab-item>
+            </v-tabs>
         </v-flex>
     </v-layout>
 </v-container>
@@ -15,7 +39,11 @@ export default {
     name: 'Admin',
     data() {
         return {
-            username: ''
+            currentTab: 'users',
+            tabs: [
+                { name: 'users', title: 'Users', data: [] },
+                { name: 'groups', title: 'Groups', data: [] }
+            ]
         }
     },
     mounted() {
@@ -27,10 +55,27 @@ export default {
         ).catch(error => {
             console.log(error)
             this.$router.push('/login')
+        }).then(() => {
+            this.getGroups()
         })
     },
     methods: {
-        
+        getUsers() {
+            this.$http.get(
+
+            )
+        },
+        getGroups() {
+            this.$http.get(
+                '/api/admin/group',
+                {
+                    headers: {'Authorization': 'Bearer ' + this.$store.state.auth.user.token}
+                }
+            ).then(resp => {
+                console.log(resp.data)
+                this.tabs[1].data = resp.data.groups
+            })
+        }
     }
 }
 </script>
